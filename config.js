79679053f153deca15608cff5ed72e6a2102bdde
Frank Kaumba <engeezgamer@gmail.com>
2025-05-18 22:14:01 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+
+global.owner = [
+  "265xxxxxxxxxxx", //should start with country code
+  ""  //second number if available
+]
+
+
+let fs = require('fs')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update ${__filename}`)
+delete require.cache[file]
+require(file) 
+})
