2ad3b19899296f6dfa2fc1cb1e61b83012608efe
Frank Kaumba <engeezgamer@gmail.com>
2025-05-18 22:44:55 +0200

    Update updater.js

--- commands/updater.js
+++ commands/updater.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-    name: 'updater',
+    name: 'update',
     description: 'Check for updates from the GitHub repo and update the bot system',
     permission: 2,
     cooldowns: 10,
